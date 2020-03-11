083e7d23928569254e4911a76022c0ae26e2bb86
Ubuntu <ubuntu@ip-172-31-35-118.us-east-2.compute.internal>
2020-03-11 02:54:20 +0000

    Updated Task 0

--- task0.py
+++ task0.py
@@ -3,4 +3,7 @@ from datasets.factory import get_imdb
 import pdb
 
 imdb = get_imdb('voc_2007_trainval')
+annotations = imdb._load_pascal_annotation(imdb._image_index[2020])
+#pt = imdb._load_image_set_index()
+image_path = imdb.image_path_from_index(imdb._image_index[2020])
 pdb.set_trace()
