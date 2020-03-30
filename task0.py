bc87cd4a469b4c51283fc07ff42c446925008989
Ubuntu <ubuntu@ip-172-31-35-118.us-east-2.compute.internal>
2020-03-30 00:31:23 +0000

    Visualizing Top 10 bounding_boxes

--- task0.py
+++ task0.py
@@ -18,18 +18,15 @@ annotations = imdb._load_pascal_annotation(imdb._image_index[2020])
 image_path = imdb.image_path_from_index(imdb._image_index[2020])
 im = cv2.imread(str(image_path))
 im = visualize_bboxes(im,annotations['boxes'])
-vis = visdom.Visdom(server='http://ec2-3-15-207-181.us-east-2.compute.amazonaws.com/',port='8097')
+vis = visdom.Visdom(server='http://ec2-3-16-46-86.us-east-2.compute.amazonaws.com/',port='8097')
 im = np.swapaxes(im,0,2)
 im = np.swapaxes(im,1,2)
 vis.image(im)
 
 im_new = cv2.imread(str(image_path))
-selective_search_db = imdb.selective_search_roidb()
-required_selective_search_db = selective_search_db[2020]
-
-#TODO: Get top 10 bboxes
-top_10_boxes = required_selective_search_db['boxes'][:10]
-#for i in range(10):
+gt_roidb = imdb.gt_roidb()
+selective_roi_db = imdb._load_selective_search_roidb(gt_roidb)
+top_10_boxes = selective_roi_db[2020]['boxes'][:10]
 im_new = visualize_bboxes(im_new,top_10_boxes)
 im_new = np.swapaxes(im_new,0,2)
 im_new = np.swapaxes(im_new,1,2)
