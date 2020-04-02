1f4f5c40cd08c786052482cc905b74d8fea0eb20
harshsha5 <harshbitsp@gmail.com>
2020-04-02 13:16:06 -0400

    Changing Visualization threshold

--- test.py
+++ test.py
@@ -44,7 +44,7 @@ if rand_seed is not None:
 cfg_from_file(cfg_file)
 
 
-def vis_detections(im, class_name, dets, thresh=0.8):
+def vis_detections(im, class_name, dets, thresh=0.1):
     """Visual debugging of detections."""
     for i in range(np.minimum(10, dets.shape[0])):
         bbox = tuple(int(np.round(x)) for x in dets[i, :4])
